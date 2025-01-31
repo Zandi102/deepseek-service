d253c8c206231ffb3e6984f892f5d595c1ecc73d
Alexander Pallozzi <alexanderpallozzi@gmail.com>
2025-01-31 01:17:55 -0500

    deep seek integration

--- src/routers/deep_seek_router.py
+++ src/routers/deep_seek_router.py
@@ -1,18 +1,47 @@
 
 from fastapi import APIRouter
 from fastapi_utils.cbv import cbv
-from dependency_injector.wiring import inject
+from src.entities.ollama_response import OllamaResponse
 from src.entities.deep_seek_request import DeepSeekRequest
+import ollama
+import re
 
 router = APIRouter()
 
 @cbv(router)
 class DeepSeekRouter:
+    def __init__(self):
+        self.model_version = "deepseek-r1:14b"
     
     @router.post("/deepseek/generate")
-    async def generate_response(self, request: DeepSeekRequest):
+    async def generate_response(self, request: DeepSeekRequest) -> OllamaResponse:
         try:
-            return {request.role: request.message}
+            response_object = ollama.chat(model=self.model_version, 
+                                          messages=[
+                                              {'role': request.role, 
+                                               'content': request.message},
+                                          ])
+            
+            if not response_object: 
+                raise LookupError(f'Unable to retrieve ollama response from model: {self.model_version}')
+            
+            # Extract "thinking" part
+            thoughts = re.search(r"<think>(.*?)</think>", 
+                                 response_object.message.content, 
+                                 re.DOTALL)
+            
+            ai_thoughts = thoughts.group(1).strip().replace('\n', '') if thoughts else ""
+
+            # Extract "speaking" part (everything after </think>)
+            ai_response = re.search(r"</think>\s*(.*)",
+                                    response_object.message.content,
+                                    re.DOTALL)
+            
+            ai_response = ai_response.group(1).strip().replace('\n', '') if ai_response else ""
+            
+            return OllamaResponse(thought_process=ai_thoughts, 
+                                  response=ai_response)
         
-        except Exception:
-            raise
+        except Exception as ex:
+            print(str(ex))
+            raise ex
